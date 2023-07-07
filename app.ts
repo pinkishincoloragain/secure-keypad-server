f196a5dc93f95588a7a758f415e5c6d217f26b92
pinkishincoloragain <smb1103@gmail.com>
2023-07-07 18:38:27 +0900

    feat: added fetch date logging console.log

--- app.ts
+++ app.ts
@@ -32,6 +32,8 @@ app.use((req: Request, res: Response, next) => {
 
 // Route handler for '/api/users'
 app.get('/api/users', verifyToken, (req: Request, res: Response) => {
+    const date = new Date();
+    console.log(date.toLocaleTimeString() + ' - GET /api/users');
     res.json(users);
 });
 
@@ -39,6 +41,8 @@ app.get('/api/user/:id', verifyToken, (req: Request, res: Response) => {
     const id = +req.params.id;
     const user = users.find(user => user.userId === id);
     if (user) {
+        const date = new Date();
+        console.log(date.toLocaleTimeString() + ` - GET /api/user/${id}`);
         res.json(user);
     } else {
         res.status(404).json({error: `User ${id} not found`});
